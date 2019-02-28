91225ce6edccd4f792190592c480cc5bd62615a7
Achyuth Batter <achyuth.bhatter@gmail.com>
2019-02-28 10:59:28 -0500

    initial

---
+++ app_server/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const ctrlLocations = require('../controllers/locations'); 
+const ctrlOthers = require('../controllers/others');
+
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location/:locationid', ctrlLocations.locationInfo); 
+router
+    .route('/location/:locationid/review/new') 
+    .get(ctrlLocations.addReview)
+    .post(ctrlLocations.doAddReview);
+
+// router.get('/location/review/new', ctrlLocations.addReview); 
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
+module.exports = router;
